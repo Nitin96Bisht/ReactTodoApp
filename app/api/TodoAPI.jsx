c5d86468577edf265dd3b13f584f9066d7bcd345
Nitin96Bisht <nitin.bisht96@gmail.com>
2017-09-11 22:24:00 +0530

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -18,5 +18,33 @@ module.exports = {
     }
 
     return $.isArray(todos) ? todos : [];
+  },
+  filterTodos: function (todos, showCompleted, searchText) {
+    var filteredTodos = todos;
+
+    //filter by show completed
+    filteredTodos = filteredTodos.filter((todos) => {
+      return !todos.completed || showCompleted;
+    });
+
+    //filter by search text
+    filteredTodos = filteredTodos.filter((todos) => {
+      var text = todos.text.toLowerCase();
+      return searchText.length === 0 || text.indexOf(searchText) > -1;
+    });
+
+    //sort todos with non-completed first
+    filteredTodos.sort((a, b) => {
+      if (!a.completed && b.completed) {
+        return -1;
+      } else if(a.completed && !b.completed) {
+        return 1;
+      }
+      else {
+        return 0;
+      }
+    });
+
+    return filteredTodos;
   }
 };
