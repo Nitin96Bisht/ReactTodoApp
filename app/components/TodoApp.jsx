9c04d4bb79f5619c143dbd19b2916256c4bc4cf2
Nitin96Bisht <nitin.bisht96@gmail.com>
2017-09-12 19:11:28 +0530

    Add completed and created timestaamp

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,5 +1,6 @@
 var React = require('react');
 var uuid = require('node-uuid');
+var moment = require('moment');
 
 var TodoList = require('TodoList');
 var AddTodo = require('AddTodo');
@@ -7,55 +8,60 @@ var TodoSearch = require('TodoSearch');
 var TodoAPI = require('TodoAPI');
 
 var TodoApp = React.createClass({
-	getInitialState: function () {
-		return {
-			showCompleted: false,
-			searchText: '',
-			todos: TodoAPI.getTodos()
-		};
-	},
-	componentDidUpdate: function () {
-		TodoAPI.setTodos(this.state.todos);
-	},
-	handleAddTodo: function (text) {
-		this.setState({
-			todos: [
-				...this.state.todos, 
-				{
-					id: uuid(),
-					text: text,
-					completed: false
-				}
-			]
-		});
-	},
-	handleToggle: function (id) {
-		var updatedTodos = this.state.todos.map((todo) => {
-			if (todo.id === id) {
-				todo.completed = !todo.completed;
-			}
-			return todo;
-		});
-		this.setState({todos: updatedTodos});
-	},
-	handleSearch: function (showCompleted, searchText) {
-		this.setState({
-			showCompleted: showCompleted,
-			searchText: searchText.toLowerCase()
-		});
-	},
-	render: function () {
-		var {todos, showCompleted, searchText} = this.state;
-		var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
+  getInitialState: function () {
+    return {
+      showCompleted: false,
+      searchText: '',
+      todos: TodoAPI.getTodos()
+    };
+  },
+  componentDidUpdate: function () {
+    TodoAPI.setTodos(this.state.todos);
+  },
+  handleAddTodo: function (text) {
+    this.setState({
+      todos: [
+        ...this.state.todos,
+        {
+          id: uuid(),
+          text: text,
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
+        }
+      ]
+    });
+  },
+  handleToggle: function (id) {
+    var updatedTodos = this.state.todos.map((todo) => {
+      if (todo.id === id) {
+        todo.completed = !todo.completed;
+        todo.completedAt = todo.completed ? moment().unix() : undefined;
+      }
 
-		return (
-			<div>
-				<TodoSearch onSearch={this.handleSearch}/>
-				<TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
-				<AddTodo onAddTodo={this.handleAddTodo}/>
-			</div>
-		);
-	}
+      return todo;
+    });
+
+    this.setState({todos: updatedTodos});
+  },
+  handleSearch: function (showCompleted, searchText) {
+    this.setState({
+      showCompleted: showCompleted,
+      searchText: searchText.toLowerCase()
+    });
+  },
+  render: function () {
+    var {todos, showCompleted, searchText} = this.state;
+    var filteredTodos = TodoAPI.filterTodos(todos, showCompleted, searchText);
+
+    return (
+      <div>
+        <TodoSearch onSearch={this.handleSearch}/>
+        <TodoList todos={filteredTodos} onToggle={this.handleToggle}/>
+        <AddTodo onAddTodo={this.handleAddTodo}/>
+      </div>
+    )
+  }
 });
 
 module.exports = TodoApp;
--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -19,13 +19,16 @@ describe('TodoApp', () => {
 		todoApp.handleAddTodo(todoText);
 
 		expect(todoApp.state.todos[0].text).toBe(todoText);
+		expect(todoApp.state.todos[0].createdAt).toBeA('number');
 	});
 
 	it('should toggle complete value when handleToggle called', () => {
 		var todoData = {
 			id: 11,
 			text: 'Test features',
-			completed: false
+			completed: false,
+			createdAt: 0,
+			completedAt: undefined
 		};
 
 		var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
@@ -34,6 +37,25 @@ describe('TodoApp', () => {
 		expect(todoApp.state.todos[0].completed).toBe(false);
 		todoApp.handleToggle(11);
 		expect(todoApp.state.todos[0].completed).toBe(true);  
+		expect(todoApp.state.todos[0].completedAt).toBeA('number');
+	});
 
+	//Test that when toggle from true to false, completedAt gets removed
+	it('should toggle todo from completed to incompleted', () => {
+		var todoData = {
+			id: 11,
+			text: 'Test features',
+			completed: true,
+			createdAt: 0,
+			completedAt: 123
+		};
+
+		var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+		todoApp.setState({todos: [todoData]});
+
+		expect(todoApp.state.todos[0].completed).toBe(true);
+		todoApp.handleToggle(11);
+		expect(todoApp.state.todos[0].completed).toBe(false);  
+		expect(todoApp.state.todos[0].completedAt).toNotExist();
 	});
 });
