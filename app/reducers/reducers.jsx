a088a41e42e373af7b81ba63d9d38f51fd8df729
Nitin96Bisht <nitin.bisht96@gmail.com>
2017-09-27 20:11:25 +0530

    Add todos reducers and tests

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,3 +1,6 @@
+var uuid = require('node-uuid');
+var moment = require('moment');
+
 export var searchTextReducer = (state = '', action) => {
 	switch (action.type) {
 		case 'SET_SEARCH_TEXT':
@@ -17,4 +20,36 @@ export var showCompletedReducer = (state = false, action) => {
       default:
       return state;
     };
+};
+
+export var todosReducer = (state = [], action) => {
+	switch (action.type) {
+		case 'ADD_TODO':
+		return [
+			...state,
+			{
+	          id: uuid(),
+	          text: action.text,
+	          completed: false,
+	          createdAt: moment().unix(),
+	          completedAt: undefined
+        }
+		];
+
+		case 'TOGGLE_TODO':
+		return state.map((todo) => {
+			if (todo.id === action.id) {
+				var nextCompleted = !todo.completed;
+
+				return {
+					...todo,
+					completed: nextCompleted,
+					completedAt: nextCompleted ? moment.unix() : undefined
+				};
+			}
+		});
+
+		default:
+		return state;
+	}
 };
