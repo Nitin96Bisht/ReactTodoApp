a1cea5c586810227f8ff0afdda200c6b9d582506
Nitin96Bisht <nitin.bisht96@gmail.com>
2017-09-28 11:08:20 +0530

    Refactor app to work with localstorage

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -56,5 +56,24 @@ describe('Reducers', () => {
 	      expect(res[0].completed).toEqual(false);
 	      expect(res[0].completedAt).toEqual(undefined);
     	});
+
+    	it('should add existing todos', () => {
+	      var todos = [{
+	        id: '111',
+	        text: 'Something',
+	        completed: false,
+	        createdAt: undefined,
+	        completedAt: 33000
+	      }];
+	      var action = {
+	        type: 'ADD_TODOS',
+	        todos
+	      };
+	      var res = reducers.todosReducer(df([]), df(action));
+
+	      expect(res.length).toEqual(1);
+	      expect(res[0]).toEqual(todos[0]);
+    	});
+
 	});
 });
